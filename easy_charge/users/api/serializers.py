ae9d44a11e9d9e7cf4347ba8b5f349708bd1d21d
Mojtaba <mghasemi13786@gmail.com>
2024-07-25 15:13:04 +0000

    register vendor user

--- easy_charge/users/api/serializers.py
+++ easy_charge/users/api/serializers.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from easy_charge.users.models import User
+from easy_charge.users.models import VendorProfile
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
-
+        fields = ["id", "username", "name", "password"]
         extra_kwargs = {
-            "url": {"view_name": "api:user-detail", "lookup_field": "username"},
+            "password": {
+                "required": True,
+                "write_only": True,
+            },
         }
+
+    def validate_password(self, data):
+        return make_password(data)
