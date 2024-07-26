ed74044dd41d6d561e979fe726b1bc5877effc05
Mojtaba <mghasemi13786@gmail.com>
2024-07-26 21:30:49 +0000

    add verify vendor task for run on celery

--- easy_charge/reseller/tasks.py
+++ easy_charge/reseller/tasks.py
@@ -1,1 +1,10 @@
-# TODO: write a task for authorise vendor at 00:00
+from celery import shared_task
+
+from .models import Vendor
+
+
+@shared_task()
+def verify_vendor():
+    for vendor in Vendor.objects.filter(is_verify=False):
+        vendor.is_verify = True
+        vendor.save()
